5f30e560a2fc6878c6d6b4f477653401b192c869
BenLampson <58603432@qq.com>
2020-04-14 18:34:34 +0800

    first version of Input number
    first version of Input number

--- DevTool/src/components/Demo.tsx
+++ DevTool/src/components/Demo.tsx
@@ -7,61 +7,24 @@ import "./demo.less"
 
 
 
-import { Tooltip, Button } from 'antd';
+import { InputNumber } from 'antd';
 
-const text = <span>prompt text</span>;
+function onChange(value: any) {
+  console.log('changed', value);
+}
 
-const buttonWidth = 70;
+
+
+
 export class Demo extends Component<any, any>{
- 
+
   render() {
     return (
-    <div className="demo" style={{marginLeft:200,marginTop:100}}>
-    <div style={{ marginLeft: buttonWidth, whiteSpace: 'nowrap' }}>
-      <Tooltip placement="topLeft" title={text}>
-        <Button>TL</Button>
-      </Tooltip>
-      <Tooltip placement="top" title={text}>
-        <Button>Top</Button>
-      </Tooltip>
-      <Tooltip placement="topRight" title={text}>
-        <Button>TR</Button>
-      </Tooltip>
-    </div>
-    <div style={{ width: buttonWidth, float: 'left' }}>
-      <Tooltip placement="leftTop" title={text}>
-        <Button>LT</Button>
-      </Tooltip>
-      <Tooltip placement="left" title={text}>
-        <Button>Left</Button>
-      </Tooltip>
-      <Tooltip placement="leftBottom" title={text}>
-        <Button>LB</Button>
-      </Tooltip>
-    </div>
-    <div style={{ width: buttonWidth, marginLeft: buttonWidth * 4 + 24 }}>
-      <Tooltip placement="rightTop" title={text}>
-        <Button>RT</Button>
-      </Tooltip>
-      <Tooltip placement="right" title={text}>
-        <Button>Right</Button>
-      </Tooltip>
-      <Tooltip placement="rightBottom" title={text}>
-        <Button>RB</Button>
-      </Tooltip>
-    </div>
-    <div style={{ marginLeft: buttonWidth, clear: 'both', whiteSpace: 'nowrap' }}>
-      <Tooltip placement="bottomLeft" title={text}>
-        <Button>BL</Button>
-      </Tooltip>
-      <Tooltip placement="bottom" title={text}>
-        <Button>Bottom</Button>
-      </Tooltip>
-      <Tooltip placement="bottomRight" title={text}>
-        <Button>BR</Button>
-      </Tooltip>
-    </div>
-  </div>
+      <div className="site-input-number-wrapper">
+        <InputNumber size="large" min={1} max={100000} defaultValue={3} onChange={onChange} />
+        <InputNumber min={1} max={100000} defaultValue={3} onChange={onChange} />
+        <InputNumber size="small" min={1} max={100000} defaultValue={3} onChange={onChange} />
+      </div>
     )
   }
 }
---
+++ src/LuanNiao.Blazor.Component.Antd/InputNumber/LNInputNumber.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LuanNiao.Blazor.Component.Antd.InputNumber
+{
+    public partial class LNInputNumber
+    {
+
+        private const string _defaultStaticClassName = "ant-input-number";
+        private const string _disabledClassName = "ant-input-number-disabled";
+        private const string _inputStaticClassName = "ant-input-number-input";
+        private const string _lgSizeClassName = "ant-input-number-lg";
+        private const string _smallSizeClassName = "ant-input-number-sm";
+        private double _value = 0;
+
+        [Parameter]
+        public ComponentSize Size { get; set; } = ComponentSize.Middle;
+
+        [Parameter]
+        public double Min { get; set; } = Double.MinValue;
+
+        [Parameter]
+        public double Step { get; set; } = 1;
+
+        [Parameter]
+        public bool Disabled { get; set; }
+
+        [Parameter]
+        public double Max { get; set; } = Double.MaxValue;
+
+        [Parameter]
+        public double DefaultValue { get; set; } = 100;
+
+        [Parameter]
+        public Action<int> OnChange { get; set; }
+        [Parameter]
+        public double Value { get => _value; set { } }
+
+
+        public LNInputNumber()
+        {
+            this._classHelper.SetStaticClass(_defaultStaticClassName);
+        }
+
+
+        protected override void OnParametersSet()
+        {
+            base.OnParametersSet();
+            this._classHelper
+                .AddOrRemove(_disabledClassName, condition: () => Disabled);
+        }
+
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            HandleDefaultValue();
+            HandleSize();
+        }
+
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            base.OnAfterRender(firstRender);
+
+            if (firstRender)
+            {
+                BindSpanClickEvent();
+            }
+        }
+
+
+
+        [JSInvokable]
+        public void OnUpClicked()
+        {
+#if DEBUG
+            Console.WriteLine(nameof(OnUpClicked));
+#endif
+            if (_value == Max)
+            {
+                return;
+            }
+            _value += Step;
+            this.Flush();
+        }
+
+
+        [JSInvokable]
+        public void OnDownClicked()
+        {
+#if DEBUG
+            Console.WriteLine(nameof(OnDownClicked));
+#endif
+            if (_value == Min)
+            {
+                return;
+            }
+            _value -= Step;
+            this.Flush();
+        }
+
+
+        private void BindSpanClickEvent()
+        {
+            ElementInfo.BindClickEvent($"up_{this.IdentityKey}", nameof(OnUpClicked), this, true);
+            ElementInfo.BindClickEvent($"down_{this.IdentityKey}", nameof(OnDownClicked), this, true);
+        }
+
+        private void HandleSize()
+        {
+            switch (Size)
+            {
+                case ComponentSize.Large:
+                    _classHelper.AddCustomClass(_lgSizeClassName);
+                    break;
+                case ComponentSize.Small:
+                    _classHelper.AddCustomClass(_smallSizeClassName);
+                    break;
+                case ComponentSize.Middle:
+                default:
+                    break;
+            }
+        }
+
+        private void HandleDefaultValue()
+        {
+            this._value = DefaultValue;
+
+            if (Max < Min)
+            {
+                throw new ArgumentException($"The max value {Max} lower than min value {Min};");
+            }
+
+            if (DefaultValue > Max)
+            {
+                this._value = Max;
+            }
+            if (DefaultValue < Min)
+            {
+                this._value = Min;
+            }
+        }
+
+
+
+    }
+}
