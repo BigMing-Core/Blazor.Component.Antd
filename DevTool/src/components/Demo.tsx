3d7a6f472ae66e812cdf2c1ebc8549f5d2453590
BenLampson(赵元源) <58603432@qq.com>
2020-04-14 22:15:53 +0800

    add the layout 2 demo
    add the layout 2 demo

--- DevTool/src/components/Demo.tsx
+++ DevTool/src/components/Demo.tsx
@@ -7,24 +7,96 @@ import "./demo.less"
 
 
 
-import { InputNumber } from 'antd';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
 
-function onChange(value: any) {
-  console.log('changed', value);
-}
-
-
-
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
 
 export class Demo extends Component<any, any>{
 
   render() {
     return (
-      <div className="site-input-number-wrapper">
-        <InputNumber size="large" min={1} max={100000} defaultValue={3} onChange={onChange} />
-        <InputNumber min={1} max={100000} defaultValue={3} onChange={onChange} />
-        <InputNumber size="small" min={1} max={100000} defaultValue={3} onChange={onChange} />
-      </div>
+      <Layout>
+        <Header className="header">
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Header>
+        <Layout>
+          <Sider width={200} className="site-layout-background">
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={['1']}
+              defaultOpenKeys={['sub1']}
+              style={{ height: '100%', borderRight: 0 }}
+            >
+              <SubMenu
+                key="sub1"
+                title={
+                  <span>
+                    <UserOutlined />
+                  subnav 1
+                </span>
+                }
+              >
+                <Menu.Item key="1">option1</Menu.Item>
+                <Menu.Item key="2">option2</Menu.Item>
+                <Menu.Item key="3">option3</Menu.Item>
+                <Menu.Item key="4">option4</Menu.Item>
+              </SubMenu>
+              <SubMenu
+                key="sub2"
+                title={
+                  <span>
+                    <LaptopOutlined />
+                  subnav 2
+                </span>
+                }
+              >
+                <Menu.Item key="5">option5</Menu.Item>
+                <Menu.Item key="6">option6</Menu.Item>
+                <Menu.Item key="7">option7</Menu.Item>
+                <Menu.Item key="8">option8</Menu.Item>
+              </SubMenu>
+              <SubMenu
+                key="sub3"
+                title={
+                  <span>
+                    <NotificationOutlined />
+                  subnav 3
+                </span>
+                }
+              >
+                <Menu.Item key="9">option9</Menu.Item>
+                <Menu.Item key="10">option10</Menu.Item>
+                <Menu.Item key="11">option11</Menu.Item>
+                <Menu.Item key="12">option12</Menu.Item>
+              </SubMenu>
+            </Menu>
+          </Sider>
+          <Layout style={{ padding: '0 24px 24px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>Home</Breadcrumb.Item>
+              <Breadcrumb.Item>List</Breadcrumb.Item>
+              <Breadcrumb.Item>App</Breadcrumb.Item>
+            </Breadcrumb>
+            <Content
+              className="site-layout-background"
+              style={{
+                padding: 24,
+                margin: 0,
+                minHeight: 280,
+              }}
+            >
+              Content
+          </Content>
+          </Layout>
+        </Layout>
+      </Layout>
     )
   }
 }
