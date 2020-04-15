d34cbfa260428ed891f3be33553d4fd776cd90c7
BenLampson(赵元源) <58603432@qq.com>
2020-04-15 22:20:58 +0800

    Add new demo
    Layout7 the antd's lastest layout type

--- DevTool/src/components/Demo.tsx
+++ DevTool/src/components/Demo.tsx
@@ -6,9 +6,21 @@ import "./demo.less"
 
 
 
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Menu } from 'antd';
+import {
+  AppstoreOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  ShopOutlined,
+  TeamOutlined,
+  UserOutlined,
+  UploadOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
 
-const { Header, Content, Footer } = Layout;
+const { Header, Content, Footer, Sider } = Layout;
+
+
 
 
 export class Demo extends Component<any, any>{
@@ -16,27 +28,155 @@ export class Demo extends Component<any, any>{
 
   render() {
     return (
-      <div id="components-layout-demo-fixed">
+      <div id="components-layout-demo-fixed-sider">
         <Layout>
-          <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+          <Sider
+            style={{
+              overflow: 'auto',
+              height: '100vh',
+              position: 'fixed',
+              left: 0,
+            }}
+          >
             <div className="logo" />
-            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-              <Menu.Item key="1">nav 1</Menu.Item>
-              <Menu.Item key="2">nav 2</Menu.Item>
-              <Menu.Item key="3">nav 3</Menu.Item>
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+              <Menu.Item key="1">
+                <UserOutlined />
+                <span className="nav-text">nav 1</span>
+              </Menu.Item>
+              <Menu.Item key="2">
+                <VideoCameraOutlined />
+                <span className="nav-text">nav 2</span>
+              </Menu.Item>
+              <Menu.Item key="3">
+                <UploadOutlined />
+                <span className="nav-text">nav 3</span>
+              </Menu.Item>
+              <Menu.Item key="4">
+                <BarChartOutlined />
+                <span className="nav-text">nav 4</span>
+              </Menu.Item>
+              <Menu.Item key="5">
+                <CloudOutlined />
+                <span className="nav-text">nav 5</span>
+              </Menu.Item>
+              <Menu.Item key="6">
+                <AppstoreOutlined />
+                <span className="nav-text">nav 6</span>
+              </Menu.Item>
+              <Menu.Item key="7">
+                <TeamOutlined />
+                <span className="nav-text">nav 7</span>
+              </Menu.Item>
+              <Menu.Item key="8">
+                <ShopOutlined />
+                <span className="nav-text">nav 8</span>
+              </Menu.Item>
             </Menu>
-          </Header>
-          <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
-            <Breadcrumb style={{ margin: '16px 0' }}>
-              <Breadcrumb.Item>Home</Breadcrumb.Item>
-              <Breadcrumb.Item>List</Breadcrumb.Item>
-              <Breadcrumb.Item>App</Breadcrumb.Item>
-            </Breadcrumb>
-            <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
-              Content
+          </Sider>
+          <Layout className="site-layout" style={{ marginLeft: 200 }}>
+            <Header className="site-layout-background" style={{ padding: 0 }} />
+            <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+              <div className="site-layout-background" style={{ padding: 24, textAlign: 'center' }}>
+                ...
+          <br />
+          Really
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          long
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          content
         </div>
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+          </Layout>
         </Layout>
       </div>
     )
