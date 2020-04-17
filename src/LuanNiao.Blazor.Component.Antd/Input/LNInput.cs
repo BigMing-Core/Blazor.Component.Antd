3919c6d6d66ac9761a8f630e8d9e282fcad900eb
BenLampson <58603432@qq.com>
2020-04-17 18:04:45 +0800

    First version of input
    with base operation

---
+++ src/LuanNiao.Blazor.Component.Antd/Input/LNInput.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LuanNiao.Blazor.Component.Antd.Input
+{
+    public partial class LNInput
+    {
+
+        private const string _staticClassName = "ant-input";
+        private const string _disabledClassName = "ant-input-disabled";
+
+
+        [Parameter]
+        public string Placeholder { get; set; }
+
+        [Parameter]
+        public bool Disabled { get; set; }
+
+        public LNInput()
+        {
+            _classHelper.SetStaticClass(_staticClassName);
+        }
+
+
+        protected override void OnParametersSet()
+        {
+            base.OnParametersSet();
+            HandleDisabled();
+            this.Flush();
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            HandleDisabled();
+        }
+
+        private void HandleDisabled()
+        {
+            _classHelper.AddOrRemove(_disabledClassName, () => Disabled);
+        }
+    }
+}
