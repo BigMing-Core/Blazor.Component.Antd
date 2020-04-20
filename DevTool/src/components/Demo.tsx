08adcd33abfda259d8352631526d3367e2496309
lucio <cuixiaolu@foxmail.com>
2020-04-21 00:25:24 +0800

    add simple spin template

--- DevTool/src/components/Demo.tsx
+++ DevTool/src/components/Demo.tsx
@@ -1,24 +1,34 @@
-import * as React from "react"
-import { Component } from "react"
-import * as ReactDOM from "react-dom"
-import "antd/dist/antd.css"
-import "./demo.less"
+import * as React from "react";
+import { Component } from "react";
+import * as ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+import "./demo.less";
 
-import { Slider } from 'antd';
-export class Demo extends Component<any, any>{
+import { Slider, Spin } from "antd";
+export class Demo extends Component<any, any> {
   state = {
     disabled: false,
   };
 
-  handleDisabledChange = (disabled:any) => {
+  handleDisabledChange = (disabled: any) => {
     this.setState({ disabled });
   };
 
   render() {
     const { disabled } = this.state;
-    return ( 
-    <Slider min={0} max={100} step={0.001}  />
-
-    )
+    return (
+      <Spin
+        size="large"
+        wrapperClassName="aaa"
+        className="bbbss"
+        tip="bbs"
+        delay={2}
+        spinning={true}
+        style={{
+          width: 40,
+        }}
+        //indicator={<div>aaa</div>}
+      />
+    );
   }
 }
