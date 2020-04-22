1db39bd125519dddfe6295c04971f2794f8b4d9e
BenLampson <58603432@qq.com>
2020-04-22 16:26:16 +0800

    add the card feature
    add the card feature

--- DevTool/src/components/Demo.tsx
+++ DevTool/src/components/Demo.tsx
@@ -4,38 +4,33 @@ import * as ReactDOM from "react-dom";
 import "antd/dist/antd.css";
 import "./demo.less";
 
-import { Drawer, Button, Spin, Alert, Switch } from "antd";
+
+
+
+
+
+import { Card } from 'antd';
+
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+
+
+
 
 export class Demo extends Component<any, any> {
-  state = { visible: false ,loading: false };
-  showDrawer = () => {
-    this.setState({
-      visible: true,
-    });
-  };
-
-  onClose = () => {
-    this.setState({
-      visible: false,
-    });
-  };
-
-  toggle = (value:boolean) => {
-    this.setState({ loading: value });
-  };
+ 
   render() {
-    return (<div>
-      <Spin spinning={this.state.loading} size="small" delay={2000}>
-        {/* <Alert
-          message="Alert message title"
-          description="Further details about the context of this alert."
-          type="info"
-        /> */}
-      </Spin>
-      <div style={{ marginTop: 16 }}>
-        Loading state：
-        <Switch checked={this.state.loading} onChange={this.toggle} />
-      </div>
-    </div>);
+    return (
+      <Card title="Default size card" extra={<a href="#">More</a>} style={{ width: 300 }}
+      actions={[
+        <SettingOutlined key="setting" />,
+        <EditOutlined key="edit" />,
+        <EllipsisOutlined key="ellipsis" />,
+      ]}
+      >
+      <p>Card content</p>
+      <p>Card content</p>
+      <p>Card content</p>
+    </Card>
+    );
   }
 }
---
+++ src/LuanNiao.Blazor.Component.Antd/Card/LNCard.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LuanNiao.Blazor.Component.Antd.Card
+{
+    public partial class LNCard
+    {
+
+        private const string _staticClassName = "ant-card";
+        private const string _borderedClassName = "ant-card-bordered";
+
+
+        public LNCard()
+        {
+            _classHelper.SetStaticClass(_staticClassName);
+        }
+
+
+        [Parameter]
+        public bool Bordered { get; set; } = true;
+
+        [Parameter]
+        public RenderFragment Title { get; set; }
+
+        [Parameter]
+        public RenderFragment Extra { get; set; }
+
+        [Parameter]
+        public IList<RenderFragment> Actions { get; set; }
+
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            HandleBordered();
+        }
+
+
+        private void HandleBordered()
+        {
+            _classHelper.AddCustomClass(_borderedClassName, () => Bordered);
+        }
+    }
+}
