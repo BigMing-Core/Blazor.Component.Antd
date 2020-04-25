12d3976bb87dd8816c6d631ab158ef0ba9220766
lucio <cuixiaolu@foxmail.com>
2020-04-26 00:38:29 +0800

    add select templete

--- DevTool/src/components/Demo.tsx
+++ DevTool/src/components/Demo.tsx
@@ -4,26 +4,33 @@ import * as ReactDOM from "react-dom";
 import "antd/dist/antd.css";
 import "./demo.less";
 
-import { Card, Avatar, Button } from 'antd';
+import { Card, Avatar, Button, Select } from 'antd';
 
 import { EditOutlined, EllipsisOutlined, SettingOutlined, UserOutlined } from '@ant-design/icons';
 
+const { Option } = Select;
 export class Demo extends Component<any, any> {
  
   render() {
     return (
       <div>
-   <Avatar icon={<UserOutlined />} />
-    <Avatar>U</Avatar>
-    <Avatar>
-      <Button>User</Button>
-
-    </Avatar>
-    <Avatar icon={<UserOutlined />}  src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"  alt="bbbbb"/>
-    <Avatar style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}>U</Avatar>
-    <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} />
- 
-    <Avatar alt="user3"></Avatar>
+   <Select defaultValue="lucy" mode="tags" style={{ width: 120 }}>
+      <Option value="jack">Jack</Option>
+      <Option value="lucy">Lucy</Option>
+      <Option value="disabled" disabled>
+        Disabled
+      </Option>
+      <Option value="Yiminghe">yiminghe</Option>
+    </Select>
+    <Select defaultValue="lucy" style={{ width: 120 }} disabled>
+      <Option value="lucy">Lucy</Option>
+    </Select>
+    <Select defaultValue="lucy" style={{ width: 120 }} loading>
+      <Option value="lucy">Lucy</Option>
+    </Select>
+    <Select defaultValue="lucy" style={{ width: 120 }} allowClear>
+      <Option value="lucy">Lucy</Option>
+    </Select>
   </div>
     );
   }
---
+++ src/LuanNiao.Blazor.Component.Antd/Select/LNSelect.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LuanNiao.Blazor.Component.Antd.Select
+{
+    public partial class LNSelect
+    {
+        public enum SMode
+        {
+            Single,
+            Multiple,
+            Tags
+        }
+        /// <summary>
+        /// Whether to show the drop-down arrow
+        /// </summary>
+        [Parameter]
+        public bool ShowArrow { get; set; } = true;
+        /// <summary>
+        /// Set mode of Select
+        /// </summary>
+        [Parameter]
+        public SMode Mode { get; set; }
+        public LNSelect()
+        {
+            _classHelper.SetStaticClass("ant-select");
+        }
+        protected override Task OnInitializedAsync()
+        {
+            HandleShowArrow();
+            return base.OnInitializedAsync();
+        }
+        protected override Task OnParametersSetAsync()
+        {
+            return base.OnParametersSetAsync();
+        }
+        protected override Task OnAfterRenderAsync(bool firstRender)
+        {
+            return base.OnAfterRenderAsync(firstRender);
+        }
+
+        private void HandleShowArrow()
+        {
+            _classHelper.AddOrRemove("ant-select-show-arrow", condition: () => ShowArrow);
+        }
+    }
+}
