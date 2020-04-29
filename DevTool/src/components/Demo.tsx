4aebcd06d9b735917b5ae8391976f59f1bb7aaea
BenLampson(赵元源) <58603432@qq.com>
2020-04-29 21:05:20 +0800

    fix demo
    fix demo

--- DevTool/src/components/Demo.tsx
+++ DevTool/src/components/Demo.tsx
@@ -6,32 +6,51 @@ import "./demo.less";
 
 
 import { Input } from 'antd';
-import { UserOutlined,SettingOutlined  } from '@ant-design/icons'
+import { AudioOutlined } from '@ant-design/icons';
 
+const { Search } = Input;
 
+const suffix = (
+  <AudioOutlined
+    style={{
+      fontSize: 16,
+      color: '#1890ff',
+      paddingRight: 4,
+    }}
+  />
+);
 export class Demo extends Component<any, any> {
 
-
+ 
   render() {
 
     return (
       <div>
-        <div style={{ marginBottom: 16 }}>
-          <Input addonBefore="http://" addonAfter=".com" defaultValue="mysite" />
-        </div>
-      
-        <div style={{ marginBottom: 16 }}>
-          <Input addonAfter={<SettingOutlined />} defaultValue="mysite" />
-        </div>
-        <div style={{ marginBottom: 16 }}>
-          <Input addonBefore="http://"    suffix=".com" defaultValue="mysite" />
-        </div>
-        
-        <div style={{ marginBottom: 16 }}>
-          <Input addonBefore="http://"  addonAfter=".com"  suffix=".com" defaultValue="mysite" />
-        </div>
-
-        <Input prefix="￥" suffix="RMB" />
+      <Search
+      placeholder="input search text"
+      onSearch={value => console.log(value)}
+      style={{ width: 200 }}
+    />
+    <br />
+    <br />
+    <Search placeholder="input search text" onSearch={value => console.log(value)} enterButton />
+    <br />
+    <br />
+    <Search
+      placeholder="input search text"
+      enterButton="Search"
+      size="large"
+      onSearch={value => console.log(value)}
+    />
+    <br />
+    <br />
+    <Search
+      placeholder="input search text"
+      enterButton="Search"
+      size="large"
+      suffix={suffix}
+      onSearch={value => console.log(value)}
+    />
       </div>
 
     );
