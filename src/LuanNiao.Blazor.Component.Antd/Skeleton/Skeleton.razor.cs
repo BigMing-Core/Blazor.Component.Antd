c1d4ab4e4bcf3c93d2a0daa618ed7bd1a15b41a0
shenbo1 <784639285@qq.com>
2020-05-09 00:24:29 +0800

    add skeleton element enum title enum paragraph attribute

--- src/LuanNiao.Blazor.Component.Antd/Skeleton/Skeleton.razor.cs
+++ src/LuanNiao.Blazor.Component.Antd/Skeleton/Skeleton.razor.cs
@@ -9,43 +9,33 @@ namespace LuanNiao.Blazor.Component.Antd.Skeleton
 {
     public partial class Skeleton
     {
-        private readonly ClassNameHelper _avatarClassNameHelper = new ClassNameHelper().AddCustomClass(_avatartDefaultClass);
+        private readonly ClassNameHelper _elementClassNameHelper = new ClassNameHelper();
 
 
         private const string _defaultClass = "ant-skeleton";
         private const string _activeClass = "ant-skeleton-active";
-        private const string _avatartDefaultClass = "ant-skeleton-avatar";
-        private const string _avatarLgClass = "ant-skeleton-avatar-lg";
-        private const string _avatarSmClass = "ant-skeleton-avatar-sm";
-        private const string _avatarSquareClass = "ant-skeleton-avatar-square";
-        private const string _avatarCircleClass = "ant-skeleton-avatar-circle";
+        private const string _elementClass = "ant-skeleton-element";
 
-        public enum ESize { 
-            Default ,
-            Large,
-            Small
-        }
-        public enum Eshape {
-            Square,
-            Circle
-        }
-        public class SkeletonAvatarProps
-        {
-            public bool Active { get; set; }
-            public ESize Size { get; set; }
-            public Eshape Shape { get; set; }
-        }
+        private string _elementDefaultClass = "ant-skeleton-{0}";
+        private string _elementLgClass = "{0}-lg";
+        private string _elementSmClass = "{0}-sm";
+        private string _elementSquareClass = "{0}-square";
+        private string _elementCircleClass = "{0}-circle";
 
         [Parameter]
         public bool Active { get; set; }
-
         [Parameter]
-
-        public SkeletonAvatarProps Avatar { get; set; }
-        private string AvatarClassName { get => this._avatarClassNameHelper.Build(); }
-        private bool _isAvatar = false;
-
-
+        public ESize Size { get; set; }
+        [Parameter]
+        public Eshape Shape { get; set; }
+        [Parameter]
+        public EElement Element { get; set; }
+        public bool IsElement { get => TitleWidth == 0 && Paragraph == null; }
+        private string ElementClassName { get => this._elementClassNameHelper.Build(); }
+        [Parameter]
+        public decimal TitleWidth { get; set; }
+        [Parameter]
+        public SkeletonParagraphProps Paragraph { get; set; }
         [Parameter]
         public bool Loading { get; set; }
         public Skeleton()
@@ -54,42 +44,49 @@ namespace LuanNiao.Blazor.Component.Antd.Skeleton
         }
         protected override void OnInitialized()
         {
-            _classHelper.AddOrRemove(_activeClass, condition: () => Active);
-            HandleAvatar();
+            _classHelper.AddOrRemove(_elementClass, condition: () => IsElement)
+            .AddOrRemove(_activeClass, condition: () => Active);
+            HandleElement();
         }
 
-        private void HandleAvatar()
+        private void HandleElement()
         {
-            if (Avatar != null)
+            if (Element != EElement.Default)
             {
-                _isAvatar = true;
-                switch (Avatar.Size)
-                {
-                    case ESize.Default:
-                        break;
-                    case ESize.Large:
-                        _avatarClassNameHelper.AddCustomClass(_avatarLgClass);
-                        break;
-                    case ESize.Small:
-                        _avatarClassNameHelper.AddCustomClass(_avatarSmClass);
-                        break;
-                    default:
-                        break;
-                }
-                switch (Avatar.Shape)
-                {
-                    case Eshape.Square:
-                        _avatarClassNameHelper.AddCustomClass(_avatarSquareClass);
-                        break;
-                    case Eshape.Circle:
-                        _avatarClassNameHelper.AddCustomClass(_avatarCircleClass);
-                        break;
-                    default:
-                        break;
-                }
+                _elementDefaultClass = string.Format(_elementDefaultClass, Element.ToString().ToLower());
+                _elementClassNameHelper.AddCustomClass(_elementDefaultClass);
+                _elementLgClass = string.Format(_elementLgClass, _elementDefaultClass);
+                _elementSmClass = string.Format(_elementSmClass, _elementDefaultClass);
+                _elementSquareClass = string.Format(_elementSquareClass, _elementDefaultClass);
+                _elementCircleClass = string.Format(_elementCircleClass, _elementDefaultClass);
+            }
+
+            switch (Size)
+            {
+                case ESize.Default:
+                    break;
+                case ESize.Large:
+                    _elementClassNameHelper.AddCustomClass(_elementLgClass);
+                    break;
+                case ESize.Small:
+                    _elementClassNameHelper.AddCustomClass(_elementSmClass);
+                    break;
+                default:
+                    break;
+            }
+            switch (Shape)
+            {
+                case Eshape.Square:
+                    _elementClassNameHelper.AddCustomClass(_elementSquareClass);
+                    break;
+                case Eshape.Circle:
+                    _elementClassNameHelper.AddCustomClass(_elementCircleClass);
+                    break;
+                default:
+                    break;
             }
         }
-        
+
     }
   
 }
