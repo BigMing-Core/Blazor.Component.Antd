9384726ce65023fb3f9ae603fe4b9f33d6954f6c
作超 韩 <284126074@qq.com>
2020-05-08 22:24:02 +0800

    Add SimpleList

---
+++ src/LuanNiao.Blazor.Component.Antd/List/ListItemExtra.razor.cs
@@ -0,0 +1,12 @@
+﻿namespace LuanNiao.Blazor.Component.Antd.List
+{
+    public partial class ListItemExtra
+    {
+        private const string _listItemExtraClassName = "ant-list-item-extra";
+
+        public ListItemExtra()
+        {
+            _classHelper.SetStaticClass(_listItemExtraClassName);
+        }
+    }
+}
---
+++ src/LuanNiao.Blazor.Component.Antd/List/SimpleList.razor.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LuanNiao.Blazor.Core.Common;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace LuanNiao.Blazor.Component.Antd.List
+{
+    public partial class SimpleList<TItem>
+    {
+        private const string _basicClassName = "ant-list";
+        private const string _splitClassName = "ant-list-split";
+        private const string _borderedClassName = "ant-list-bordered";
+        private const string _notEmptyAfterLastItemClassName = "ant-list-something-after-last-item";
+        private const string _smallClassName = "ant-list-sm";
+        private const string _largeClassName = "ant-list-lg";
+        private const string _gridClassName = "ant-list-grid";
+
+        private const string _headerClassName = "ant-list-header";
+        private const string _footerClassName = "ant-list-footer";
+
+        private const string _spinLoadingClassName = "ant-spin-nested-loading";
+        private const string _spinContainerClassName = "ant-spin-container";
+
+        private const string _listItemBasicClassName = "ant-list-item";
+        private const string _noFlexClassName = "ant-list-item-no-flex";
+
+        private string _listItemClassName = _listItemBasicClassName;
+
+        [Parameter]
+        public LNListSize Size { get; set; } = LNListSize.Default;
+
+        /// <summary>
+        /// Whether to have a border
+        /// </summary>
+        [Parameter]
+        public bool Bordered { get; set; } = true;
+
+        /// <summary>
+        /// Whether to have dividing line
+        /// </summary>
+        [Parameter]
+        public bool Split { get; set; } = true;
+
+        /// <summary>
+        /// Whether to display in raster
+        /// </summary>
+        [Parameter]
+        public bool Grid { get; set; }
+
+        [Parameter]
+        public string Header { get; set; }
+
+        [Parameter]
+        public bool NoFlex { get; set; }
+
+        [Parameter]
+        public string Footer { get; set; }
+
+        [Parameter]
+        public List<TItem> Items { get; set; }
+
+        [Parameter]
+        public RenderFragment<TItem> ListItem { get; set; }
+
+        [Parameter]
+        public Action<TItem> OnClick { get; set; }
+
+        public SimpleList()
+        {
+            _classHelper.SetStaticClass(_basicClassName);
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            BorderHandler();
+            SplitHandler();
+            SizeHandler();
+            NotEmptyAfterLastItemHandler();
+            NoFlexHandle();
+        }
+
+        private void BorderHandler()
+        {
+            _classHelper.AddOrRemove(_borderedClassName, () => Bordered);
+        }
+
+        private void SplitHandler()
+        {
+            _classHelper.AddOrRemove(_splitClassName, () => Split);
+        }
+
+        private void GridHandler()
+        {
+            _classHelper.AddOrRemove(_gridClassName, () => Grid);
+        }
+
+        private void NotEmptyAfterLastItemHandler()
+        {
+            _classHelper.AddOrRemove(_notEmptyAfterLastItemClassName, () => Footer != null);
+        }
+
+        private void NoFlexHandle()
+        {
+            if (NoFlex)
+            {
+                _listItemClassName += " " + _noFlexClassName;
+            }
+        }
+
+        private void SizeHandler()
+        {
+            switch (Size)
+            {
+                case LNListSize.Large:
+                    _classHelper.AddCustomClass(_largeClassName);
+                    break;
+                case LNListSize.Small:
+                    _classHelper.AddCustomClass(_smallClassName);
+                    break;
+            }
+        }
+
+        private void OnClickHandler(TItem item)
+        {
+            OnClick?.Invoke(item);
+        }
+    }
+}
