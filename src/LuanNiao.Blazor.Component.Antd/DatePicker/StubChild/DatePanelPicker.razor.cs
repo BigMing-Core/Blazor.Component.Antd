4f16377b7eec4efe3384139159455bc49c11d22b
BenLampson(赵元源) <58603432@qq.com>
2020-05-09 00:09:53 +0800

    add new component
    add datepanel picker

---
+++ src/LuanNiao.Blazor.Component.Antd/DatePicker/StubChild/DatePanelPicker.razor.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LuanNiao.Blazor.Component.Antd.DatePicker.StubChild
+{
+    public partial class DatePanelPicker
+    { 
+        [Parameter]
+        public int CurrentMonth { get; set; } = DateTime.Now.Month;
+
+        [Parameter]
+        public int CurrentYear { get; set; } = DateTime.Now.Year;
+        [Parameter]
+        public int CurrentDay { get; set; } = DateTime.Now.Day;
+
+        [Parameter]
+        public DateTime CurrentSelectDate { get; set; } = DateTime.Now;
+
+        
+
+ 
+        [Parameter]
+        public Action<(int year, int month, int week)> ItemSelected { get; set; }
+
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            Core.Translater.CultureChanged += OnCultureChanged;
+        }
+
+        private void OnCultureChanged(string _)
+        {
+            this.Flush();
+        }
+
+
+        protected override void Dispose(bool flag)
+        {
+            base.Dispose(flag);
+            Core.Translater.CultureChanged -= OnCultureChanged;
+        }
+
+        private void OnClickItem(DateTime date)
+        {
+            this.CurrentSelectDate = date;
+            this.Flush();
+        }
+
+
+        private void PrevMonth()
+        {
+            if (CurrentMonth - 1 <= 0)
+            {
+                return;
+            }
+            CurrentMonth -= 1;
+            this.Flush();
+        }
+        private void NextMonth()
+        {
+            if (CurrentMonth + 1 >= 13)
+            {
+                return;
+            }
+            CurrentMonth += 1;
+            this.Flush();
+        }
+        private void PrevYear()
+        {
+            if (CurrentYear - 1 <= 1)
+            {
+                return;
+            }
+            CurrentYear -= 1;
+            this.Flush();
+        }
+        private void NextYear()
+        {
+            if (CurrentYear + 1 >= 7000)
+            {
+                return;
+            }
+            CurrentYear += 1;
+            this.Flush();
+        }
+    }
+}
