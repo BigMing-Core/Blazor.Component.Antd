7001098f2f4040958a6e7d9e0e7dff483bff8e61
BenLampson <58603432@qq.com>
2020-05-08 10:01:21 +0800

    add new feature:List
    add new feature:List

---
+++ src/LuanNiao.Blazor.Component.Antd/List/ListItem.razor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Components;
+
+namespace LuanNiao.Blazor.Component.Antd.List
+{
+    public partial class ListItem
+    {
+        private const string _listItemClassName                = "ant-list-item";
+        private const string _listItemMainClassName            = "ant-list-item-main";
+        private const string _listItemExtraClassName           = "ant-list-item-extra";
+        private const string _listItemActionClassName          = "ant-list-item-action";
+
+        [Parameter]
+        public RenderFragment Meta { get; set; }
+
+        [Parameter]
+        public RenderFragment Content { get; set; }
+
+        [Parameter]
+        public RenderFragment Extra { get; set; }
+
+        [Parameter]
+        public RenderFragment Action { get; set; }
+    }
+}
