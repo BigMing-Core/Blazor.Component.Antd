cb9c6676428eb3b6d0cd9c66e579a7d0e654d35f
shenbo1 <784639285@qq.com>
2020-05-12 17:10:58 +0800

    swtich base

---
+++ src/LuanNiao.Blazor.Component.Antd/Switch/Switch.razor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LuanNiao.Blazor.Component.Antd.Switch
+{
+    public partial class Switch
+    {
+        private const string _defaultClass = "ant-switch";
+        private const string _checkClass = "ant-switch-checked";
+        private const string _disabledClass = "ant-switch-disabled";
+        public Switch()
+        {
+            _classHelper.SetStaticClass(_defaultClass);
+        }
+
+    }
+}
