e7c031ee32b0e4b71949993ba8f892f570241640
shenbo1 <784639285@qq.com>
2020-05-12 18:13:04 +0800

    switch disabled ,checked ,size

--- src/LuanNiao.Blazor.Component.Antd/Switch/Switch.razor.cs
+++ src/LuanNiao.Blazor.Component.Antd/Switch/Switch.razor.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LuanNiao.Core;
+using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,9 +11,53 @@ namespace LuanNiao.Blazor.Component.Antd.Switch
         private const string _defaultClass = "ant-switch";
         private const string _checkClass = "ant-switch-checked";
         private const string _disabledClass = "ant-switch-disabled";
+        private const string _smallClass = "ant-switch-small";
+
+        [Parameter]
+
+        public bool Checked { get; set; }
+        [Parameter]
+        public UnionType<string,> CheckedChildren { get; set; }
+        [Parameter]
+        public bool DefaultChecked { get; set; }
+        [Parameter]
+        public bool Disabled { get; set; }
+        [Parameter]
+        public string Size { get; set; }
+        [Parameter]
+        public int UnCheckedChildren { get; set; }
+        public int CheckedValue { get; set; }
         public Switch()
         {
             _classHelper.SetStaticClass(_defaultClass);
+        }
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            HandleCheck();
+            HandleSize();
+            HandleChildren();
+        }
+
+        protected override void OnParametersSet()
+        {
+            base.OnParametersSet();
+            HandleDisabled();
+        }
+        private void HandleDisabled()
+        {
+            _classHelper.AddOrRemove(_disabledClass, condition: () => Disabled);
+        }
+        private void HandleCheck()
+        {
+            _classHelper.AddOrRemove(_checkClass, condition: () => Checked);
+        }
+        private void HandleSize() { 
+            _classHelper.AddOrRemove(_smallClass, condition: () => Size=="small");
+        }
+        private void HandleChildren()
+        {
+
         }
 
     }
