8f391545b12991ab1b80e076f68a96dd2b333fe6
shenbo1 <784639285@qq.com>
2020-05-13 23:24:53 +0800

    update switch checked  unchecked,loading,size

--- src/LuanNiao.Blazor.Component.Antd/Switch/Switch.razor.cs
+++ src/LuanNiao.Blazor.Component.Antd/Switch/Switch.razor.cs
@@ -1,5 +1,6 @@
 ﻿using LuanNiao.Core;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,21 +13,21 @@ namespace LuanNiao.Blazor.Component.Antd.Switch
         private const string _checkClass = "ant-switch-checked";
         private const string _disabledClass = "ant-switch-disabled";
         private const string _smallClass = "ant-switch-small";
-
+        private const string _loadingClass = "ant-switch-loading";
         [Parameter]
-
         public bool Checked { get; set; }
-        [Parameter]
-        public UnionType<string,> CheckedChildren { get; set; }
-        [Parameter]
-        public bool DefaultChecked { get; set; }
         [Parameter]
         public bool Disabled { get; set; }
         [Parameter]
         public string Size { get; set; }
         [Parameter]
-        public int UnCheckedChildren { get; set; }
-        public int CheckedValue { get; set; }
+        public UnionType<string, RenderFragment> CheckedChildren { get; set; }
+        [Parameter]
+        public UnionType<string, RenderFragment> UnCheckedChildren { get; set; }
+        public UnionType<string, RenderFragment> CheckedValue { get; set; }
+        [Parameter]
+        public bool Loading { get; set; }
+
         public Switch()
         {
             _classHelper.SetStaticClass(_defaultClass);
@@ -37,12 +38,34 @@ namespace LuanNiao.Blazor.Component.Antd.Switch
             HandleCheck();
             HandleSize();
             HandleChildren();
+            HandleLoading();
         }
-
         protected override void OnParametersSet()
         {
             base.OnParametersSet();
             HandleDisabled();
+        }
+        protected override void OnAfterRender(bool firstRender)
+        {
+            base.OnAfterRender(firstRender);
+            if (firstRender)
+            {
+                BindEvent();
+            }
+        }
+        private void BindEvent()
+        {
+            ElementInfo.BindClickEvent($"Swtich_{IdentityKey}", nameof(HandleOnClick), this);
+         
+        }
+        [JSInvokable]
+        public async void HandleOnClick()
+        {
+            if (this.Disabled) { return; }
+            Checked = !Checked;
+            HandleCheck();
+            HandleChildren();
+            this.Flush();
         }
         private void HandleDisabled()
         {
@@ -55,9 +78,13 @@ namespace LuanNiao.Blazor.Component.Antd.Switch
         private void HandleSize() { 
             _classHelper.AddOrRemove(_smallClass, condition: () => Size=="small");
         }
+        private void HandleLoading()
+        {
+            _classHelper.AddOrRemove(_loadingClass, condition: () => Loading);
+        }
         private void HandleChildren()
         {
-
+            CheckedValue = Checked ? CheckedChildren : UnCheckedChildren;
         }
 
     }
