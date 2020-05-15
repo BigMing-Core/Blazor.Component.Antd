9f0a40e4c5af5f837e2450ffdecbbc621ef0fa3b
BenLampson(赵元源) <58603432@qq.com>
2020-05-16 00:13:38 +0800

    new demo
    new demo

--- src/Test/TestPages/Pages/EventHubTest.razor.cs
+++ src/Test/TestPages/Pages/EventHubTest.razor.cs
@@ -1,9 +1,11 @@
 ﻿using LuanNiao.Blazor.Core;
+using LuanNiao.Blazor.Core.Common;
 using LuanNiao.Blazor.Core.ElementEventHub.Attributes;
 using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace TestPages.Pages
 {
@@ -14,15 +16,140 @@ namespace TestPages.Pages
             base.OnAfterRender(firstRender);
             if (firstRender)
             {
-                ElementEventHub.RegistInstance(this);
+                ElementEventHub.GetElementInstance("TestInfo");
+                    //.Bind(this, nameof(OnClick)
+                    //, nameof(MouseOver)
+                    //, nameof(MouseDown)
+                    //, nameof(MouseEnter)
+                    //, nameof(MouseUp)
+                    //, nameof(MouseMove)
+                    //, nameof(MouseOut)
+                    //, nameof(ContextMenu));
+                ElementEventHub.GetElementInstance("TestInfo1");
+                    //.Bind(this, nameof(OnClick)
+                    //, nameof(MouseOver1)
+                    //, nameof(MouseDown1)
+                    //, nameof(MouseEnter1)
+                    //, nameof(MouseUp1)
+                    //, nameof(MouseMove1)
+                    //, nameof(MouseOut1)
+                    //, nameof(ContextMenu1));
             }
         }
 
         
-        [OnClickEvent($"TestInfo{IdentityKey}")]
-        public void OnClick()
+
+
+        [OnClickEvent]
+        public void OnClick(MouseEvent @event)
         {
-            Console.WriteLine($"{nameof(EventHubTest)}:{nameof(OnClick)}");
+            Console.WriteLine($"{nameof(EventHubTest)}:{nameof(OnClick)}:{@event.Button}");
+        }
+
+        [OnMouseOverEvent]
+        public void MouseOver()
+        {
+            Console.WriteLine($"{nameof(EventHubTest)}:{nameof(MouseOver)}");
+        }
+
+
+        [OnMouseEnterEvent]
+        public void MouseEnter()
+        {
+            Console.WriteLine($"{nameof(EventHubTest)}:{nameof(MouseEnter)}");
+        }
+
+
+        [OnMouseDownEvent]
+        public void MouseDown()
+        {
+            Console.WriteLine($"{nameof(EventHubTest)}:{nameof(MouseDown)}");
+        }
+
+
+        [OnMouseUpEvent]
+        public void MouseUp()
+        {
+            Console.WriteLine($"{nameof(EventHubTest)}:{nameof(MouseUp)}");
+        }
+
+
+        [OnMouseMoveEvent]
+        public void MouseMove()
+        {
+            Console.WriteLine($"{nameof(EventHubTest)}:{nameof(MouseMove)}");
+        }
+
+
+        [OnMouseOutEvent]
+        public void MouseOut()
+        {
+            Console.WriteLine($"{nameof(EventHubTest)}:{nameof(MouseOut)}");
+        }
+
+
+        [OnContextMenuEvent]
+        public void ContextMenu()
+        {
+            Console.WriteLine($"{nameof(EventHubTest)}:{nameof(ContextMenu)}");
+        }
+
+
+
+
+
+        [OnClickEvent]
+        public void OnClick1(MouseEvent @event)
+        {
+            Console.WriteLine($"{nameof(EventHubTest)}:{nameof(OnClick1)}:{@event.Button}");
+        }
+
+        [OnMouseOverEvent]
+        public void MouseOver1()
+        {
+            Console.WriteLine($"{nameof(EventHubTest)}:{nameof(MouseOver1)}");
+        }
+
+
+        [OnMouseEnterEvent]
+        public void MouseEnter1()
+        {
+            Console.WriteLine($"{nameof(EventHubTest)}:{nameof(MouseEnter1)}");
+        }
+
+
+        [OnMouseDownEvent]
+        public void MouseDown1()
+        {
+            Console.WriteLine($"{nameof(EventHubTest)}:{nameof(MouseDown1)}");
+        }
+
+
+        [OnMouseUpEvent]
+        public void MouseUp1()
+        {
+            Console.WriteLine($"{nameof(EventHubTest)}:{nameof(MouseUp1)}");
+        }
+
+
+        [OnMouseMoveEvent]
+        public void MouseMove1()
+        {
+            Console.WriteLine($"{nameof(EventHubTest)}:{nameof(MouseMove1)}");
+        }
+
+
+        [OnMouseOutEvent]
+        public void MouseOut1()
+        {
+            Console.WriteLine($"{nameof(EventHubTest)}:{nameof(MouseOut1)}");
+        }
+
+
+        [OnContextMenuEvent]
+        public void ContextMenu1()
+        {
+            Console.WriteLine($"{nameof(EventHubTest)}:{nameof(ContextMenu1)}");
         }
     }
 }
