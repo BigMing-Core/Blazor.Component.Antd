bd632c509c6650458da44235026739108bee4bc7
作超 韩 <284126074@qq.com>
2020-05-17 22:17:56 +0800

    BackTop可监听指定Id的Dom

--- src/LuanNiao.Blazor.Component.Antd/BackTop/BackTop.Razor.cs
+++ src/LuanNiao.Blazor.Component.Antd/BackTop/BackTop.Razor.cs
@@ -1,17 +1,17 @@
 ﻿using LuanNiao.Blazor.Core;
 using LuanNiao.Blazor.Core.Common;
+using LuanNiao.Blazor.Core.ElementEventHub.Attributes;
 using Microsoft.AspNetCore.Components;
-using Microsoft.JSInterop;
 using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 
 namespace LuanNiao.Blazor.Component.Antd.BackTop
 {
     public partial class BackTop
     {
         private const string _basicClassName = "ant-back-top";
+
+        private bool _isWindowEvent;
 
         /// <summary>
         /// 滚动高度达到此值才出现BackTop，默认400
@@ -23,7 +23,7 @@ namespace LuanNiao.Blazor.Component.Antd.BackTop
         /// 要监听元素的值，可能是dom类型或dom的id
         /// </summary>
         [Parameter]
-        public string Target { get; set; } = "window";
+        public string Target { get; set; }
 
         private string _childClassName;
 
@@ -31,19 +31,53 @@ namespace LuanNiao.Blazor.Component.Antd.BackTop
 
         public BackTop()
         {
+            Console.WriteLine(Target);
             _classHelper.SetStaticClass(_basicClassName);
         }
 
         protected override void OnInitialized()
         {
             base.OnInitialized();
+
+            _isWindowEvent = string.IsNullOrWhiteSpace(Target) || "window".Equals(Target, System.StringComparison.OrdinalIgnoreCase);
+            Disposing += () =>
+            {
+                if (_isWindowEvent)
+                {
+                    WindowEventHub.Scrolled -= WindowEventHub_Scrolled;
+                }
+            };
+
+            // 获取要监听的元素
             WindowEventHub.Scrolled += WindowEventHub_Scrolled;
+
+            // 获取当前位置
             _styleHelper.AddCustomStyle("visibility", "hidden");
         }
 
         protected override void OnAfterRender(bool firstRender)
         {
             base.OnAfterRender(firstRender);
+            if (firstRender &&!_isWindowEvent)
+            { 
+                var target = ElementEventHub.GetElementInstance(Target.Trim());
+                target.Bind(this, nameof(ElementEventHub_Scrolled));
+            }
+        }
+
+        private void HandleListenDom()
+        {
+            if(string.IsNullOrWhiteSpace(Target) || Target.Trim().ToLower() == "window")
+            {
+                // 监听窗体
+                _isWindowEvent = true;
+                WindowEventHub.Scrolled += WindowEventHub_Scrolled;
+            }
+            else
+            {
+                // 监听dom
+                _isWindowEvent = false;
+            }
         }
 
         private void HandleVisibily(bool visiable)
@@ -57,9 +91,8 @@ namespace LuanNiao.Blazor.Component.Antd.BackTop
             );
 
             _childClassName = visiable ? "fade-enter fade-enter-active" : "fade-leave fade-leave-active";
-            Flush();
-
             _visiable = visiable;
+            Flush();
         }
 
         private void WindowEventHub_Scrolled(WindowScrollEvent e)
@@ -68,10 +101,11 @@ namespace LuanNiao.Blazor.Component.Antd.BackTop
             HandleVisibily(visiable);
         }
 
-        protected override void Dispose(bool flag)
+        [OnScrollEvent]
+        public void ElementEventHub_Scrolled(ElementScrollEvent e)
         {
-            WindowEventHub.Scrolled -= WindowEventHub_Scrolled;
-            base.Dispose(flag);
+            var visiable = e.ScrollTop > VisibilityHeight;
+            HandleVisibily(visiable);
         }
     }
 }
